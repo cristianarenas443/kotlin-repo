6e10a1bb6f4babe5f71c2978df6e5b3b4585db7d
Cristian Arenas <83226149+caarenas97@users.noreply.github.com>
2022-04-08 15:45:10 -0500

    :sparkles: Kotlin repo inicializado
    Inicialización de repositorio de Kotlin con metodo main.

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,12 @@
+
+
+/*
+    Funcion principal de Kotlin
+    Desc: main recibe una lista de argumentos como cadenas de caracteres y ejecuta el codigo interno.
+    Type: void
+    Return: none
+*/
+
+fun main(args: Array<String>) {
+    println("Hola mundo de Kotlin")
+}
