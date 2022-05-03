7fa3a74cedac1c1bb0aaa7220d8b3706beef4b5f
Cristian Arenas <caarenas@ibm.com>
2022-05-02 21:06:40 -0500

    :sparkles: String to JSON
    Se integra funcionalidad de lectura de archivos y de conversion de String to Json

---
+++ src/main/kotlin/global.kt
@@ -0,0 +1,16 @@
+
+/*
+    global.kt -> clase global
+    Creado por: Ing.Cristian Arenas
+    Desc: Clase global con ejemplos de tipos de datos nativos de Kotlin
+*/
+
+class global {
+    val CHAR = 'C'
+    val STRING = "STRING"
+    val INTEGER = 123
+    val DOUBLE = 23.1616161616161616
+    val FLOAT = 1.4f
+    val LONG = 3000000000
+    val BYTE: Byte = 1
+}
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,12 +1,45 @@
+import com.google.gson.*
+import java.io.File
 
 
 /*
-    Funcion principal de Kotlin
-    Desc: main recibe una lista de argumentos como cadenas de caracteres y ejecuta el codigo interno.
+    Kotlin-repo
+    Creado por: Ing.Cristian Arenas
+    Fecha: 08/04/2022
+    Repositorio público de Kotlin para desarrolladores.
+*/
+
+/*
+    Funcion : main
+    Input: Array<String>
+    Output: none
     Type: void
-    Return: none
+    Desc: main recibe una lista de argumentos como cadenas de caracteres y ejecuta el codigo interno.
 */
 
 fun main(args: Array<String>) {
-    println("Hola mundo de Kotlin")
+    val file = File("src/res/resources.json").readText()
+    val jsonElement = getJsonElement(file)
+    print(jsonElement?.get("data"))
+
+}
+
+
+/*
+    Funcion: getJsonElement
+    Input: json : String
+    Output: JsonObject?
+    Type: fun
+    Desc: getJsonElement recibe un string con el objeto json y devuelve un JsonObject
+ */
+
+fun getJsonElement(json : String) : JsonObject? {
+    var jsonObject : JsonObject? = null
+    try {
+        jsonObject = JsonParser().parse(json).asJsonObject
+    }catch (e : JsonSyntaxException){
+        print("*** ERROR_PARSE_JSON: " + e.message)
+    }finally {
+        return jsonObject
+    }
 }
